93fbaa73614cb105991d5ea91c35a0f897ff21c0
Alex Radon <svirelmuz@gmail.com>
2021-02-21 20:14:31 +0300

    добавлены:  функция minus  переменная text

--- first/nums.py
+++ first/nums.py
@@ -7,6 +7,12 @@ def plus(a, b):
     # сложение чисел a и  b
     return a + b
 
+text = 'new text' # текст ни о чем просто для изменения программы
+
+def minus(a, b):
+    # вычитание чисел
+    return a - b
 
 if __name__ == '__main__':
     print(plus(2, 2))
+    print(minus(4, 2))
